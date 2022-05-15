b1f19b82f09631339860c92300239ed4dca90cb1
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 19:57:23 -0400

    Agregar el proyecto

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Joseph Camilo Brito");
+            Console.WriteLine("¿Como estas?");
+            Console.WriteLine("Buenos Dias");
+            Console.WriteLine("Feliz Navidad!");
